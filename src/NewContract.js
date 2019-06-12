6fb6ec7506ebd43dbcea0eec1627cbdc4138f6fb
Wynne Barsanti <wgb5gc@virginia.edu>
2019-06-12 13:38:50 -0400

    created newcontract page

---
+++ src/NewContract.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import CompanyNavbar from './CompanyNavbar'
+
+class NewContract extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <CompanyNavbar/>
+            </div>
+        )
+    }
+
+}
+
+export default NewContract;
