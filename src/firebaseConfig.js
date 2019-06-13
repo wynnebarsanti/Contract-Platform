1058120c918f9e3feeb40bac0c1ec017471dabba
Theo Rose <terose73@gmail.com>
2019-06-13 16:01:22 -0400

    not pushing private information to github

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,15 +1,13 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDkLGUnZve8qOIQmLgTPTBhrG2p5KMiM0M",
-  authDomain: "contract-platform.firebaseapp.com",
-  databaseURL: "https://contract-platform.firebaseio.com",
-  projectId: "contract-platform",
-  storageBucket: "contract-platform.appspot.com",
-  messagingSenderId: "946312988132",
-  appId: "1:946312988132:web:372c245cd22f7fc2"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID
 };
-
 firebase.initializeApp(firebaseConfig);
 
 export default firebase;
